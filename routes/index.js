3609d8ffb2c41f713fe7195a7f19e214b84fee73
tashinoki <kenya.112163@gmail.com>
2019-01-25 14:49:39 +0900

    first

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,27 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
-//
-// const con = mysql.createConnection({
-//     host: "127.0.0.1",
-//     user: "root",
-//     password: "fMxJUM6G",
-//     database: "test",
-// });
+
+const con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "fMxJUM6G",
+    database: "test",
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //
-  // const sql = 'select * from `test`';
-  //
-  //   con.connect(function(err) {
-  //       if (err) throw err;
-  //       con.query(sql, function (err, result) {
-  //           if (err) throw err;
-  //           console.log("Result: " + result);
-  //           res.render('index', {title: result[0]['test']})
-  //       });
-  //   });
-    res.render('index', {title: 'Express'});
+
+  const sql = 'select * from `test`';
+
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Result: " + result);
+            res.render('index', {title: result[0]['test']})
+        });
+    });
 
 });
 
