12d6e87ef46398a885bc1c337663e987ea554e78
tashinoki <kenya.112163@gmail.com>
2019-01-25 16:40:44 +0900

    add

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const {PythonShell} = require('python-shell');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    const option  = {mode: 'text'};
+    const option  = {mode: 'text', args: [3, 3]};
 
     PythonShell.run('./routes/sample.py', option, (err, result) => {
 
--- routes/sample.py
+++ routes/sample.py
@@ -1,2 +1,7 @@
 
-print('hello python');
+import sys
+
+num1 = int(sys.argv[1])
+num2 = int(sys.argv[2])
+
+print(num1 + num2)
